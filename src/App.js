286fe62b83ec52c6859567884976e97a31eeaaf1
guilhermelucasfreitas <guilherme.pereira39@fatec.sp.gov.br>
2020-02-18 00:21:21 -0300

    subindo projeto

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import Header from './Header';
+import Generos from './Generos'
+import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
+  const Home = () => {
+    return <h1>Home</h1>
+  }
+  
+
+  function App() {
+
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+      axios.get('/api').then (res => {
+        setData(res.data)
+      })
+    }, [])
+
+    return (
+      <Router>
+      <div>
+        <Header/>
+        <Route path="/" exact component={Home}/>
+        <Route path="/generos" component={Generos}/>
+         <pre>{JSON.stringify(data)}</pre> 
+      </div>
+      </Router>
+      );
+    }
+    
+    export default App;
+    
---
+++ src/Generos.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const Generos = () => {
+    const [data,setData] = useState([])
+    useEffect(() => {
+        axios.get('api/genres')
+        .then( res => {
+            setData(res.data.data)
+        })
+    }, [])
+    return (
+    <div>
+        <h1>Generos</h1>
+        <table className="table table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Ação</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                </tr>
+            </tbody>
+        </table>
+
+         <pre>{JSON.stringify(data)}</pre>
+    </div>
+    )
+}
+    
+    export default Generos
+    
