bfdcaae511cdf531b9ac351b7681d25e438b1f85
Guilherme Pereira <guilherme.pereira@hubbrasil.local>
2020-02-21 17:30:52 -0200

    adicionando series

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import Header from './Header';
-import Generos from './Generos'
-import NovoGenero from './NovoGenero'
-import EditarGenero from './EditarGenero'
+import Generos from './Generos';
+import NovoGenero from './NovoGenero';
+import EditarGenero from './EditarGenero';
+import Series from './Series';
+
 import axios from 'axios';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom'
 
   const Home = () => {
@@ -27,10 +30,13 @@ import {
       <Router>
       <div>
         <Header/>
-        <Route path="/" exact component={Home}/>
-        <Route path="/generos/:id" exact component={EditarGenero}/>
-        <Route path="/generos/novo" exact component={NovoGenero}/>
-        <Route path="/generos" exact component={Generos}/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/generos" exact component={Generos}/>
+          <Route path="/generos/novo" exact component={NovoGenero}/>
+          <Route path="/generos/:id" exact component={EditarGenero}/>
+          <Route path="/series" exact component={Series}/>
+        </Switch>
          {/* <pre>{JSON.stringify(data)}</pre>  */}
       </div>
       </Router>
